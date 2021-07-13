2840e30807e81f86d9f60aaafccf2872283fc2aa
uberbrent <bmjohns1337@gmail.com>
2021-07-13 00:03:12 -0500

    mapped out additional components, filled out initial code for nav and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Created by Brent M Johnson</p>
+        </footer>
+    )
+}
---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+    return(
+        <header>
+            <h1>
+                <a href='/'>
+                    <span>brent.m.johnson</span>
+                </a>
+            </h1>
+            <nav>
+                <ul className="flex-row">
+                    <li>
+
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
